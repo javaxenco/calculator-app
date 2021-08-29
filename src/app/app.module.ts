101fa1fc3d1201b6fd430de244fe895b51d76b8d
Davit <dajavakhishvili@tbcbank.com.ge>
2021-08-30 03:41:23 +0400

    main theme design model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { CalcHeaderComponent } from './calculator/calc-header/calc-header.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    CalcHeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
